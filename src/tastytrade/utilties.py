15d0bc047511e7f95d8e8b3015f1fe2b8a30e27a
Xavier Mandeng <mandeng@gmail.com>
2024-11-12 15:33:29 -0500

    mvp refactor

--- src/tastytrade/utilties.py
+++ src/tastytrade/utilties.py
@@ -6,6 +6,6 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 
-def response_to_class(data: dict[str, Any]) -> SimpleNamespace:
+def dict_to_class(data: dict[str, Any]) -> SimpleNamespace:
     clean_data = {k.replace("-", "_"): v for k, v in data.items()}
     return SimpleNamespace(**clean_data)
