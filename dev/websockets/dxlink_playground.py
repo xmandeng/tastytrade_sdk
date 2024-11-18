86c8ea91988ef956befdbbdfac5fa0b3dfbb1da4
Xavier Mandeng <mandeng@gmail.com>
2024-11-18 12:47:08 -0500

    dxlink prototype

--- dev/websockets/dxlink_playground.py
+++ dev/websockets/dxlink_playground.py
@@ -13,19 +13,7 @@ from tastytrade import Credentials
 from tastytrade.session import AsyncSessionHandler
 from tastytrade.utilties import setup_logging
 
-SETUP = json.dumps(
-    {
-        "type": "SETUP",
-        "channel": 0,
-        "version": "0.1-DXF-JS/0.3.0",
-        "keepaliveTimeout": 60,
-        "acceptKeepaliveTimeout": 60,
-    }
-)
-
-CHANNEL_REQUEST = json.dumps(
-    {"type": "CHANNEL_REQUEST", "channel": 3, "service": "FEED", "parameters": {"contract": "AUTO"}}
-)
+logger = logging.getLogger(__name__)
 
 
 def timestamp():
@@ -34,39 +22,60 @@ def timestamp():
 
 async def main():
     setup_logging(logging.INFO)
+    logger.info("\nLets get this started ...\n")
 
-    session = await AsyncSessionHandler.create(Credentials(env="Live"))
-    token = session.session.headers["token"]
-    # finally:
-    #     await session.close_session()
+    session = await AsyncSessionHandler.create(Credentials(env="Test"))
 
-    authorize = json.dumps({"type": "AUTH", "channel": 0, "token": token})
+    setup = json.dumps(
+        {
+            "type": "SETUP",
+            "channel": 0,
+            "version": "0.1-DXF-JS/0.3.0",
+            "keepaliveTimeout": 60,
+            "acceptKeepaliveTimeout": 60,
+        }
+    )
+
+    channel_request = json.dumps(
+        {
+            "type": "CHANNEL_REQUEST",
+            "channel": 1,
+            "service": "FEED",
+            "parameters": {"contract": "AUTO"},
+        }
+    )
+
+    authorize = json.dumps(
+        {"type": "AUTH", "channel": 0, "token": session.session.headers["token"]}
+    )
 
     try:
-        async with connect("wss://tasty-openapi-ws.dxfeed.com/realtime") as websocket:
-            print("Lets get this started ...\n")
+        async with connect(session.session.headers["dxlink-url"]) as websocket:
 
-            await asyncio.wait_for(websocket.send(SETUP), timeout=5)
+            # Send SETUP message
+            await asyncio.wait_for(websocket.send(setup), timeout=5)
             reply = await asyncio.wait_for(websocket.recv(), timeout=5)
-
-            print("Received:", str(timestamp()))
-            print(f"{json.dumps(json.loads(reply), indent=2)}\n")
+            reply_data = json.loads(reply)
+            logger.info("%s", reply_data.get("type"))
 
             reply = await asyncio.wait_for(websocket.recv(), timeout=5)
-            print("Received:", str(timestamp()))
-            print(f"{json.dumps(json.loads(reply), indent=2)}\n")
+            reply_data = json.loads(reply)
+            logger.info("%s:%s", reply_data.get("type"), "" or reply_data.get("state"))
 
+            # Send AUTH message
             await asyncio.wait_for(websocket.send(authorize), timeout=5)
             reply = await asyncio.wait_for(websocket.recv(), timeout=5)
-            print("Received:", str(timestamp()))
-            print(f"{json.dumps(json.loads(reply), indent=2)}\n")
+            reply_data = json.loads(reply)
+            logger.info("%s:%s", reply_data.get("type"), "" or reply_data.get("state"))
 
-            await asyncio.wait_for(websocket.send(CHANNEL_REQUEST), timeout=5)
+            # Send CHANNEL_REQUEST message
+            await asyncio.wait_for(websocket.send(channel_request), timeout=5)
 
             while True:
                 try:
                     reply = await asyncio.wait_for(websocket.recv(), timeout=45)
-                    print("Received:", str(timestamp()))
+                    reply_data = json.loads(reply)
+                    logger.info("%s", reply_data.get("type"))
                     print(f"{json.dumps(json.loads(reply), indent=2)}\n")
                 except asyncio.TimeoutError:
                     print("Receiving operation timed out\n")
@@ -77,8 +86,10 @@ async def main():
 
             await session.close_session()
 
-    except asyncio.TimeoutError:
-        print("Operation timed out after 45 seconds\n")
+    except asyncio.TimeoutError as e:
+        print(f"Operation timed: {e}\n")
+    except KeyboardInterrupt as k:
+        logger.info(f"KeyboardInterrupt: {k}\n")
     except Exception as e:
         print(f"An error occurred: {e}\n")
 
