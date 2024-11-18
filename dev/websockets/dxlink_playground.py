ebc737de7d089263b483ab0ffd15ce2d6ed847f7
Xavier Mandeng <mandeng@gmail.com>
2024-11-18 17:37:52 -0500

    dxlink feed test

--- dev/websockets/dxlink_playground.py
+++ dev/websockets/dxlink_playground.py
@@ -5,7 +5,6 @@
 import asyncio
 import json
 import logging
-from datetime import datetime
 
 from websockets.asyncio.client import ClientConnection, connect
 
@@ -15,50 +14,7 @@ from tastytrade.utilties import setup_logging
 
 logger = logging.getLogger(__name__)
 
-
-def timestamp():
-    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-
-async def main():
-    setup_logging(logging.INFO)
-    logger.info("\nLets get this started ...\n")
-
-    session = await AsyncSessionHandler.create(Credentials(env="Test"))
-
-    try:
-        async with connect(session.session.headers["dxlink-url"]) as websocket:
-            await setup_connection(websocket)
-            await authorize_connection(websocket, session.session.headers["token"])
-            await request_channel(websocket, 1)
-
-            while True:
-                try:
-                    reply = await asyncio.wait_for(websocket.recv(), timeout=45)
-                    reply_data = json.loads(reply)
-
-                    if reply_data.get("type") == "KEEPALIVE":
-                        logger.info("KEEPALIVE [remote]")
-                        await keepalive(websocket)
-                    else:
-                        print(f"{json.dumps(json.loads(reply), indent=2)}")
-                        logger.info("%s", reply_data.get("type"))
-
-                except asyncio.TimeoutError:
-                    print("Receiving operation timed out\n")
-                    break
-                except Exception as e:
-                    print(f"An error occurred: {e}\n")
-                    break
-
-            await session.close_session()
-
-    except asyncio.TimeoutError as e:
-        print(f"Operation timed: {e}\n")
-    except KeyboardInterrupt as k:
-        logger.info(f"KeyboardInterrupt: {k}\n")
-    except Exception as e:
-        print(f"An error occurred: {e}\n")
+ENV = "Live"
 
 
 async def setup_connection(websocket: ClientConnection):
@@ -118,6 +74,7 @@ async def request_channel(websocket: ClientConnection, channel: int):
 
 
 async def channel_listener(websocket: ClientConnection):
+    # ! Needs testing
     while True:
         try:
             reply = await asyncio.wait_for(websocket.recv(), timeout=45)
@@ -135,12 +92,12 @@ async def keepalive(websocket: ClientConnection):
     logger.info("KEEPALIVE [local]")
 
 
-async def setup_feed(websocket: ClientConnection):
+async def setup_feed(websocket: ClientConnection, channel: int):
 
     feed = json.dumps(
         {
             "type": "FEED_SETUP",
-            "channel": 3,
+            "channel": channel,
             "acceptAggregationPeriod": 0.1,
             "acceptDataFormat": "COMPACT",
             "acceptEventFields": {
@@ -184,7 +141,82 @@ async def setup_feed(websocket: ClientConnection):
         }
     )
 
-    await websocket.send(json.dumps(feed))
+    await websocket.send(feed)
+
+
+async def subscribe_to_feed(websocket: ClientConnection, channel: int):
+
+    feed_subscription = json.dumps(
+        {
+            "type": "FEED_SUBSCRIPTION",
+            "channel": channel,
+            "reset": True,
+            "add": [
+                # {"type": "Trade", "symbol": "BTC/USD:CXTALP"},
+                # {"type": "Quote", "symbol": "BTC/USD:CXTALP"},
+                # {"type": "Profile", "symbol": "BTC/USD:CXTALP"},
+                # {"type": "Summary", "symbol": "BTC/USD:CXTALP"},
+                # {"type": "Trade", "symbol": "SPY"},
+                # {"type": "TradeETH", "symbol": "SPY"},  # WHY SPXY on TradeETH
+                # {"type": "Quote", "symbol": "SPX"},
+                # {"type": "Profile", "symbol": "SPY"},
+                # {"type": "Summary", "symbol": "SPY"},
+                # {"type": "Quote", "symbol": ".SPX241220P5885"},
+                # {"type": "Greeks", "symbol": ".SPX241220P5885"},
+                # {"type": "Quote", "symbol": ".SPXW241118C5885"},
+                # {"type": "Greeks", "symbol": ".SPXW241118C5885"},
+                {"type": "Quote", "symbol": ".SPXW241118P5885"},
+                {"type": "Greeks", "symbol": ".SPXW241118P5885"},
+            ],
+        }
+    )
+
+    await websocket.send(feed_subscription)
+
+
+async def main():
+    setup_logging(logging.INFO)
+    logger.info("\nLets get this started ...\n")
+
+    session = await AsyncSessionHandler.create(Credentials(env=ENV))
+
+    try:
+        async with connect(session.session.headers["dxlink-url"]) as websocket:
+            await setup_connection(websocket)
+            await authorize_connection(websocket, session.session.headers["token"])
+            await request_channel(websocket, 1)
+            await setup_feed(websocket, 1)
+            await subscribe_to_feed(websocket, 1)
+
+            while True:
+                try:
+                    reply = await asyncio.wait_for(websocket.recv(), timeout=45)
+                    reply_data = json.loads(reply)
+
+                    if reply_data.get("type") == "KEEPALIVE":
+                        logger.info("KEEPALIVE [remote]")
+                        await keepalive(websocket)
+                    elif reply_data.get("type") == "FEED_DATA":
+                        print(f"{json.dumps(json.loads(reply), indent=2)}")
+                    else:
+                        print(f"{json.dumps(json.loads(reply), indent=2)}")
+                        logger.info("%s", reply_data.get("type"))
+
+                except asyncio.TimeoutError:
+                    print("Receiving operation timed out\n")
+                    break
+                except Exception as e:
+                    print(f"An error occurred: {e}\n")
+                    break
+
+            await session.close_session()
+
+    except asyncio.TimeoutError as e:
+        print(f"Operation timed: {e}\n")
+    except KeyboardInterrupt as k:
+        logger.info(f"KeyboardInterrupt: {k}\n")
+    except Exception as e:
+        print(f"An error occurred: {e}\n")
 
 
 if __name__ == "__main__":
