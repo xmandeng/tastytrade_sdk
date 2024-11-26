9c93e582d0e6da2f0c0608b36f0b7a5457bcc5ec
Xavier Mandeng <mandeng@gmail.com>
2024-11-26 13:31:51 -0500

    Bifurcate feed from control handlers

--- src/tastytrade/sessions/messaging.py
+++ src/tastytrade/sessions/messaging.py
@@ -3,7 +3,7 @@ import logging
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from enum import Enum
-from typing import Any, Dict, List
+from typing import Any, Awaitable, Callable, Dict, List
 
 from tastytrade.sessions.parsers import EventParser
 
@@ -99,7 +99,8 @@ class MessageHandler:
         self.queue_manager = MessageQueues()
         self.tasks: List[asyncio.Task] = []
         self.shutdown = asyncio.Event()
-        self.handlers: Dict[str, BaseMessageHandler] = {
+
+        self.control_handlers: Dict[str, BaseMessageHandler] = {
             "SETUP": SetupHandler(),
             "AUTH_STATE": AuthStateHandler(),
             "FEED_CONFIG": FeedConfigHandler(),
@@ -109,11 +110,57 @@ class MessageHandler:
             "ERROR": ErrorHandler(),
         }
 
+        self.feed_handlers: Dict[str, BaseMessageHandler] = {}
+
+        # TODO Handle control signals separate from subscription feeds
+
         for channel in self.queue_manager.queues:
+
+            if channel == 0:
+                router = self.control_handler
+            else:
+                logger.info("Feed handler for channel: %s", channel)
+                continue
+
             task = asyncio.create_task(
-                self.queue_listener(channel), name=f"queue_listener_ch{channel}"
+                self.queue_listener(channel, router), name=f"queue_listener_ch{channel}"
             )
             self.tasks.append(task)
+
+    async def queue_listener(
+        self, channel: int, router: Callable[[Dict[str, Any]], Awaitable[None]]
+    ) -> None:
+        while not self.shutdown.is_set():
+            try:
+                message = await asyncio.wait_for(
+                    self.queue_manager.queues[channel].get(), timeout=1
+                )
+
+                await asyncio.wait_for(router(message), timeout=1)
+
+                self.queue_manager.queues[channel].task_done()
+
+            except asyncio.TimeoutError:
+                continue  # TODO - Check shutdown event
+            except asyncio.CancelledError:
+                logger.info("Queue listener stopped for channel %s", channel)
+                break
+            except Exception as e:
+                logger.error("Error in queue listener for channel %s: %s", channel, e)
+                break
+
+    async def control_handler(self, raw_message: Dict[str, Any]) -> None:
+        message = Message(
+            type=raw_message.get("type", "UNKNOWN"),
+            channel=raw_message.get("channel", 0),
+            data=raw_message,
+        )
+
+        if handler := self.control_handlers.get(message.type):
+            # sample message: {"type":"FEED_DATA","channel":1,"data":["Quote",["Quote",".SPXW241125P5990",10.5,10.7,4.0,24.0,"Quote",".SPXW241125P5980",5.1,5.3,96.0,55.0,"Quote",".SPXW241125P5970",2.4,2.5,63.0,116.0]]}
+            await handler.process_message(message)
+        else:
+            logger.warning("No handler found for message type: %s", message.type)
 
     async def cleanup(self) -> None:
         """Gracefully shutdown all queue listeners."""
@@ -133,36 +180,3 @@ class MessageHandler:
 
         await asyncio.gather(*self.tasks, return_exceptions=True)
         logger.info("Message handler stopped")
-
-    async def queue_listener(self, channel: int) -> None:
-        while not self.shutdown.is_set():
-            try:
-                message = await asyncio.wait_for(
-                    self.queue_manager.queues[channel].get(), timeout=1
-                )
-
-                await asyncio.wait_for(self.route_to_handler(message), timeout=1)
-
-                self.queue_manager.queues[channel].task_done()
-
-            except asyncio.TimeoutError:
-                continue  # TODO - Check shutdown event
-            except asyncio.CancelledError:
-                logger.info("Queue listener stopped for channel %s", channel)
-                break
-            except Exception as e:
-                logger.error("Error in queue listener for channel %s: %s", channel, e)
-                break
-
-    async def route_to_handler(self, raw_message: Dict[str, Any]) -> None:
-        message = Message(
-            type=raw_message.get("type", "UNKNOWN"),
-            channel=raw_message.get("channel", 0),
-            data=raw_message,
-        )
-
-        if handler := self.handlers.get(message.type):
-            # sample message: {"type":"FEED_DATA","channel":1,"data":["Quote",["Quote",".SPXW241125P5990",10.5,10.7,4.0,24.0,"Quote",".SPXW241125P5980",5.1,5.3,96.0,55.0,"Quote",".SPXW241125P5970",2.4,2.5,63.0,116.0]]}
-            await handler.process_message(message)
-        else:
-            logger.warning("No handler found for message type: %s", message.type)
