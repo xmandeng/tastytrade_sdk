6bd2e646a7fa6c970ac4f721dd6c5d717e33d420
Xavier Mandeng <44256405+xmandeng@users.noreply.github.com>
2024-11-26 23:28:05 -0500

    Segregate channel listener responsibilities (#7)

--- src/tastytrade/sessions/dxlink.py
+++ src/tastytrade/sessions/dxlink.py
@@ -6,86 +6,78 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Any, Optional
 
-from tastytrade.sessions.messages import MessageHandler
+from tastytrade.sessions.messaging import Channels, MessageHandler
 from tastytrade.sessions.sockets import WebSocketManager
 
 QueryParams = Optional[dict[str, Any]]
 
 logger = logging.getLogger(__name__)
 
-
-CHANNEL_REQUEST = {
-    "type": "CHANNEL_REQUEST",
-    "service": "FEED",
-    "parameters": {"contract": "AUTO"},
+FEED_SPECS = {
+    # "Trade": ["eventSymbol", "price", "dayVolume", "size"],
+    "Quote": [
+        "eventSymbol",
+        "bidPrice",
+        "askPrice",
+        "bidSize",
+        "askSize",
+    ],
+    # "Greeks": [
+    #     "eventSymbol",
+    #     "volatility",
+    #     "delta",
+    #     "gamma",
+    #     "theta",
+    #     "rho",
+    #     "vega",
+    # ],
+    # "Profile": [
+    #     "eventSymbol",
+    #     "description",
+    #     "shortSaleRestriction",
+    #     "tradingStatus",
+    #     "statusReason",
+    #     "haltStartTime",
+    #     "haltEndTime",
+    #     "highLimitPrice",
+    #     "lowLimitPrice",
+    #     "high52WeekPrice",
+    #     "low52WeekPrice",
+    # ],
+    # "Summary": [
+    #     "eventSymbol",
+    #     "openInterest",
+    #     "dayOpenPrice",
+    #     "dayHighPrice",
+    #     "dayLowPrice",
+    #     "prevDayClosePrice",
+    # ],
 }
+
 FEED_SETUP = {
     "type": "FEED_SETUP",
     "acceptAggregationPeriod": 0.1,
     "acceptDataFormat": "COMPACT",
-    "acceptEventFields": {
-        "Trade": ["eventType", "eventSymbol", "price", "dayVolume", "size"],
-        "Quote": [
-            "eventType",
-            "eventSymbol",
-            "bidPrice",
-            "askPrice",
-            "bidSize",
-            "askSize",
-        ],
-        "Greeks": [
-            "eventType",
-            "eventSymbol",
-            "volatility",
-            "delta",
-            "gamma",
-            "theta",
-            "rho",
-            "vega",
-        ],
-        "Profile": [
-            "eventType",
-            "eventSymbol",
-            "description",
-            "shortSaleRestriction",
-            "tradingStatus",
-            "statusReason",
-            "haltStartTime",
-            "haltEndTime",
-            "highLimitPrice",
-            "lowLimitPrice",
-            "high52WeekPrice",
-            "low52WeekPrice",
-        ],
-        "Summary": [
-            "eventType",
-            "eventSymbol",
-            "openInterest",
-            "dayOpenPrice",
-            "dayHighPrice",
-            "dayLowPrice",
-            "prevDayClosePrice",
-        ],
-    },
+    "acceptEventFields": FEED_SPECS,
 }
 SUBSCRIPTION_REQUEST = {
     "type": "FEED_SUBSCRIPTION",
     "reset": True,
     "add": [
         # {"type": "Trade", "symbol": "BTC/USD:CXTALP"},
-        # {"type": "Quote", "symbol": "BTC/USD:CXTALP"},
+        # {"type": "Trade", "symbol": "SPY"},
         # {"type": "Profile", "symbol": "BTC/USD:CXTALP"},
+        # {"type": "Profile", "symbol": "SPY"},
         # {"type": "Summary", "symbol": "BTC/USD:CXTALP"},
-        {"type": "Trade", "symbol": "SPY"},
-        {"type": "Quote", "symbol": "SPX"},
-        {"type": "Profile", "symbol": "SPY"},
-        {"type": "Summary", "symbol": "SPY"},
+        # {"type": "Summary", "symbol": "SPY"},
+        # {"type": "Greeks", "symbol": f".SPXW{datetime.now().strftime('%y%m%d')}P5990"},
+        # {"type": "Greeks", "symbol": f".SPXW{datetime.now().strftime('%y%m%d')}P5980"},
+        # {"type": "Greeks", "symbol": f".SPXW{datetime.now().strftime('%y%m%d')}P5970"},
         {"type": "Quote", "symbol": f".SPXW{datetime.now().strftime('%y%m%d')}P5990"},
-        {"type": "Greeks", "symbol": f".SPXW{datetime.now().strftime('%y%m%d')}P5990"},
         {"type": "Quote", "symbol": f".SPXW{datetime.now().strftime('%y%m%d')}P5980"},
-        {"type": "Greeks", "symbol": f".SPXW{datetime.now().strftime('%y%m%d')}P5980"},
         {"type": "Quote", "symbol": f".SPXW{datetime.now().strftime('%y%m%d')}P5970"},
-        {"type": "Greeks", "symbol": f".SPXW{datetime.now().strftime('%y%m%d')}P5970"},
+        {"type": "Quote", "symbol": "SPX"},
+        {"type": "Quote", "symbol": "BTC/USD:CXTALP"},
     ],
 }
 
@@ -95,39 +87,33 @@ class DXLinkConfig:
     keepalive_timeout: int = 60
     version: str = "0.1-DXF-JS/0.3.0"
     channel_assignment: int = 1
-    max_subscriptions: int = 10
+    max_subscriptions: int = 100
     reconnect_attempts: int = 3  # for later use
     reconnect_delay: int = 5  # for later use
 
 
 class DXLinkClient:
 
-    # @classmethod
-    # @inject
-    # def run(cls, credentials: Credentials):
-    #     instance = cls(MessageHandler())
-    #     asyncio.run(instance.connect(credentials))
-
     def __init__(
         self,
         websocket_manager: WebSocketManager,
-        message_handler: MessageHandler = MessageHandler(),
-        config: DXLinkConfig = DXLinkConfig(),
+        message_handler: Optional[MessageHandler] = None,
+        config: Optional[DXLinkConfig] = None,
     ):
         self.websocket = websocket_manager.websocket
-        self.message_handler = message_handler
+        self.message_handler = message_handler or MessageHandler()
+        config = config or DXLinkConfig()
+
         self.subscription_semaphore = Semaphore(config.max_subscriptions)
 
-    async def request_channel(self, channel: int, request: dict[str, Any] = CHANNEL_REQUEST):
-        request = request | {"channel": channel}
+    async def setup_feed(self, channel: Channels, request: dict[str, Any] = FEED_SETUP):
+        request = request | {"channel": channel.value}
         await asyncio.wait_for(self.websocket.send(json.dumps(request)), timeout=5)
 
-    async def setup_feed(self, channel: int, request: dict[str, Any] = FEED_SETUP):
-        request = request | {"channel": channel}
-        await asyncio.wait_for(self.websocket.send(json.dumps(request)), timeout=5)
-
-    async def subscribe_to_feed(self, channel: int, request: dict[str, Any] = SUBSCRIPTION_REQUEST):
-        request = request | {"channel": channel}
+    async def subscribe_to_feed(
+        self, channel: Channels, request: dict[str, Any] = SUBSCRIPTION_REQUEST
+    ):
+        request = request | {"channel": channel.value}
         async with self.subscription_semaphore:
             await asyncio.wait_for(
                 self.websocket.send(json.dumps(request)),
---
+++ src/tastytrade/sessions/messaging.py
@@ -0,0 +1,358 @@
+import asyncio
+import logging
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from decimal import Decimal
+from enum import Enum
+from itertools import chain
+from typing import Any, Awaitable, Callable, Dict, List, Union, cast
+
+from pydantic import ValidationError
+
+from tastytrade.sessions.models import (
+    GreeksEvent,
+    ProfileEvent,
+    QuoteEvent,
+    SummaryEvent,
+    TradeEvent,
+)
+
+logger = logging.getLogger(__name__)
+
+# Define more specific type aliases
+SingleEventType = Union[TradeEvent, QuoteEvent, GreeksEvent, ProfileEvent, SummaryEvent]
+EventList = List[SingleEventType]
+ParsedEventType = Union[SingleEventType, EventList]
+
+
+@dataclass
+class Message:
+    type: str
+    channel: int
+    content: dict[str, Any]
+    data: list[Any]
+
+
+class Channels(Enum):
+    Control = 0
+    Trades = 1
+    Quotes = 3
+    Greeks = 5
+    Profile = 7
+    Summary = 9
+
+
+class MessageQueues:
+    """MessageQueues is a singleton QueueManager."""
+
+    instance = None
+
+    def __new__(cls):
+        if cls.instance is None:
+            cls.instance = object.__new__(cls)
+        return cls.instance
+
+    def __init__(self) -> None:
+        self.queues: dict[int, asyncio.Queue] = {queue.value: asyncio.Queue() for queue in Channels}
+
+
+class BaseMessageHandler(ABC):
+    async def process_message(self, message: Message) -> None:
+        await self.handle_message(message)
+
+    @abstractmethod
+    async def handle_message(self, message: Message) -> None:
+        pass
+
+
+class KeepaliveHandler(BaseMessageHandler):
+    async def handle_message(self, message: Message) -> None:
+        logger.info("%s:Received", message.type)
+
+
+class ErrorHandler(BaseMessageHandler):
+    async def handle_message(self, message: Message) -> None:
+        logger.error("%s:%s", message.content.get("error"), message.content.get("message"))
+
+
+class SetupHandler(BaseMessageHandler):
+    async def handle_message(self, message: Message) -> None:
+        logger.info("%s", message.type)
+
+
+class AuthStateHandler(BaseMessageHandler):
+    async def handle_message(self, message: Message) -> None:
+        logger.info("%s:%s", message.type, message.content.get("state"))
+
+
+class ChannelOpenedHandler(BaseMessageHandler):
+    async def handle_message(self, message: Message) -> None:
+        message_type = message.type
+        channel = message.channel
+        event = message.content.get("event")
+        logger.info("%s:%s:%s", message_type, channel, event)
+
+
+class FeedConfigHandler(BaseMessageHandler):
+    async def handle_message(self, message: Message) -> None:
+        message_type = message.type
+        channel = message.channel
+        event = message.content.get("event")
+        data_format = message.content.get("dataFormat")
+        logger.info("%s:%s:%s:%s", message_type, channel, data_format, event)
+
+
+class FeedDataHandler(BaseMessageHandler):
+    async def handle_message(self, message: Message) -> None:
+        raise NotImplementedError("%s should employ event_handlers", message.type)
+
+
+class BaseEventHandler(ABC):
+    async def process_message(self, message: Message) -> None:
+        await self.handle_message(message)
+
+    @abstractmethod
+    async def handle_message(self, message: Message) -> ParsedEventType:
+        pass
+
+
+class QuotesHandler(BaseEventHandler):
+
+    async def handle_message(self, message: Message) -> ParsedEventType:
+        quotes: List[QuoteEvent] = []
+
+        feed = iter(*chain(filter(lambda x: x != "Quote", message.data)))
+        while True:
+            try:
+                quotes.append(
+                    QuoteEvent(
+                        symbol=next(feed),
+                        bid_price=next(feed),
+                        ask_price=next(feed),
+                        bid_size=next(feed),
+                        ask_size=next(feed),
+                    )
+                )
+            except StopIteration:
+                if remaining := [*feed]:
+                    raise ValueError(
+                        "Unexpected data in Quotes handler: [%s]", ", ".join(remaining)
+                    )
+                else:
+                    break
+            except ValidationError as e:
+                logger.error("Validation error in Quotes handler: %s", e)
+            except Exception as e:
+                logger.error("Unexpected error in Quotes handler: %s", e)
+
+            logger.info("Quotes handler for channel %s: %s", message.channel, quotes)
+
+        return cast(EventList, quotes)
+
+
+class TradesHandler(BaseEventHandler):
+
+    async def handle_message(self, message: Message) -> ParsedEventType:
+        logger.info("Quote handler for channel %s: %s", message.channel, message.data)
+        feed = iter(chain(filter(lambda x: x != "Quote", message.data)))
+
+        trades: List[TradeEvent] = []
+
+        while True:
+            try:
+                trades.append(
+                    TradeEvent(
+                        symbol=next(feed),
+                        price=Decimal(next(feed)),
+                        day_volume=Decimal(next(feed)),
+                        size=Decimal(next(feed)),
+                    )
+                )
+            except StopIteration:
+                break
+        return EventList(trades)
+
+
+class GreeksHandler(BaseEventHandler):
+
+    async def handle_message(self, message: Message) -> ParsedEventType:
+        logger.info("Quote handler for channel %s: %s", message.channel, message.data)
+        feed = iter(chain(filter(lambda x: x != "Quote", message.data)))
+
+        greeks: List[GreeksEvent] = []
+
+        while True:
+            try:
+                greeks.append(
+                    GreeksEvent(
+                        symbol=next(feed),
+                        volatility=Decimal(next(feed)),
+                        delta=Decimal(next(feed)),
+                        gamma=Decimal(next(feed)),
+                        theta=Decimal(next(feed)),
+                        rho=Decimal(next(feed)),
+                        vega=Decimal(next(feed)),
+                    )
+                )
+            except StopIteration:
+                break
+        return EventList(greeks)
+
+
+class ProfileHandler(BaseEventHandler):
+
+    async def handle_message(self, message: Message) -> ParsedEventType:
+        logger.info("Quote handler for channel %s: %s", message.channel, message.data)
+        feed = iter(chain(filter(lambda x: x != "Quote", message.data)))
+
+        profile: List[ProfileEvent] = []
+
+        while True:
+            try:
+                profile.append(
+                    ProfileEvent(
+                        symbol=next(feed),
+                        description=next(feed),
+                        short_sale_restriction=next(feed),
+                        trading_status=next(feed),
+                        status_reason=next(feed),
+                        halt_start_time=next(feed),
+                        halt_end_time=next(feed),
+                        high_limit_price=Decimal(next(feed)),
+                        low_limit_price=Decimal(next(feed)),
+                        high_52_week_price=Decimal(next(feed)),
+                        low_52_week_price=Decimal(next(feed)),
+                    )
+                )
+            except StopIteration:
+                break
+        return EventList(profile)
+
+
+class SummaryHandler(BaseEventHandler):
+
+    async def handle_message(self, message: Message) -> ParsedEventType:
+        logger.info("Quote handler for channel %s: %s", message.channel, message.data)
+        feed = iter(chain(filter(lambda x: x != "Quote", message.data)))
+
+        summary: List[SummaryEvent] = []
+
+        while True:
+            try:
+                summary.append(
+                    SummaryEvent(
+                        symbol=next(feed),
+                        open_interest=Decimal(next(feed)),
+                        day_open_price=Decimal(next(feed)),
+                        day_high_price=Decimal(next(feed)),
+                        day_low_price=Decimal(next(feed)),
+                        prev_day_close_price=Decimal(next(feed)),
+                    )
+                )
+            except StopIteration:
+                break
+        return EventList(summary)
+
+
+class MessageHandler:
+    queue_manager = MessageQueues()
+    tasks: List[asyncio.Task] = []
+    shutdown = asyncio.Event()
+
+    control_handlers: Dict[str, BaseMessageHandler] = {
+        "SETUP": SetupHandler(),
+        "AUTH_STATE": AuthStateHandler(),
+        "CHANNEL_OPENED": ChannelOpenedHandler(),
+        "FEED_CONFIG": FeedConfigHandler(),
+        "FEED_DATA": FeedDataHandler(),  # Not implemented
+        "KEEPALIVE": KeepaliveHandler(),
+        "ERROR": ErrorHandler(),
+    }
+
+    event_handlers = {
+        Channels.Trades.name: TradesHandler(),
+        Channels.Quotes.name: QuotesHandler(),
+        Channels.Greeks.name: GreeksHandler(),
+        Channels.Profile.name: ProfileHandler(),
+        Channels.Summary.name: SummaryHandler(),
+    }
+
+    def __init__(self) -> None:
+        for channel in self.queue_manager.queues:
+            handler = self.control_handler if channel == 0 else self.event_handler
+
+            task = asyncio.create_task(
+                self.queue_listener(channel, handler), name=f"queue_listener_ch{channel}"
+            )
+            self.tasks.append(task)
+
+    async def queue_listener(
+        self, channel: int, handler: Callable[[Message], Awaitable[None]]
+    ) -> None:
+        while not self.shutdown.is_set():
+            try:
+                reply = await asyncio.wait_for(self.queue_manager.queues[channel].get(), timeout=1)
+
+                message_type = reply.get("type", "UNKNOWN")
+                queue_name = reply.get("channel") if message_type == "FEED_DATA" else 0
+                data = reply.get("data", {})
+                content: dict[str, Any] = reply.copy()
+                content.pop("data", None)
+                content.pop("type", None)
+
+                message = Message(
+                    type=message_type,
+                    channel=queue_name,
+                    data=data,
+                    content=content,
+                )
+
+                await asyncio.wait_for(handler(message), timeout=1)
+
+                # self.queue_manager.queues[channel].task_done()
+
+            except asyncio.TimeoutError:
+                continue  # TODO - Check shutdown event
+            except asyncio.CancelledError:
+                logger.info("Queue listener stopped for channel %s", channel)
+                break
+            except Exception as e:
+                logger.error("Error in queue listener for channel %s: %s", channel, e)
+                break
+            finally:
+                self.queue_manager.queues[channel].task_done()
+
+    async def control_handler(self, message: Message) -> None:
+        """Read message headers and dispatch to the appropriate handler."""
+        if handler := self.control_handlers.get(message.type):
+            await handler.process_message(message)
+        else:
+            logger.warning("No handler found for message type: %s", message.type)
+
+    async def event_handler(self, message: Message) -> None:
+        """Read message headers and dispatch to the appropriate handler."""
+        event_type = Channels(message.channel).name
+
+        if handler := self.event_handlers.get(event_type):
+            await handler.process_message(message)
+        else:
+            logger.warning("No handler found for message type: %s", message.type)
+
+    async def cleanup(self) -> None:
+        """Gracefully shutdown all queue listeners."""
+        self.shutdown.set()
+
+        try:
+            await asyncio.wait_for(
+                asyncio.gather(*[queue.join() for queue in self.queue_manager.queues.values()]),
+                timeout=5.0,
+            )
+        except asyncio.TimeoutError:
+            logger.warning("Timeout waiting for queues to empty")
+
+        # Cancel all tasks
+        for task in self.tasks:
+            task.cancel()
+
+        await asyncio.gather(*self.tasks, return_exceptions=True)
+        logger.info("Message handlers stopped")
